83e496e6005a799c7f12306f4ac94a96ec2f8860
Charlotte Bouyat <c.bouyat.dev@gmail.com>
2026-01-09 12:18:36 +0100

    Mise à jour

--- exercices/jour-02/catalogue.php
+++ exercices/jour-02/catalogue.php
@@ -1,53 +1,71 @@
+<?php
+// starter-project/public/catalogue.php
+require_once __DIR__ . '/../app/data.php';
+// $products est maintenant disponible
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <title>Test PHP</title>
     </head>
     <body>
+
         <?php 
-$products = [
-    [
-        "name" => "T-shirt",
-        "price" => 29.99,
-        "stock" => 50
-    ],
-    [
-        "name" => "Jean",
-        "price" => 79.99,
-        "stock" => 30
-    ],
-    [
-        "name" => "Casquette",
-        "price" => 19.99,
-        "stock" => 100
-    ],
-    [
-        "name" => "Veste",
-        "price" => 49.99,
-        "stock" => 80
-    ],
-    [
-        "name" => "Echarpe",
-        "price" => 10.99,
-        "stock" => 40
-    ]
-
-];
-
 // Accéder au nom du 3e produit
-echo $products[2]["name"]; // Casquette
+echo $products[2]["Name"]; // Casquette
 
 // Accéder au prix du 1er produit
-echo $products[0]["price"]; // 29.99
+echo $products[0]["Price"]; // 29.99
 
 // Accédez au stock du dernier produit
-echo $products[4] ["stock"]; // 40
+echo $products[4] ["Stock"]; // 40
 
 // modifier la valeur du stock du 2e produit
-$products[1]["stock"]= 40;
-echo $products[1] ["stock"]; // 40
-
+$products[1]["Stock"]= 40;
+echo $products[1] ["Stock"]; // 40
         ?>
+
+
+<h1>Consultez nos produits</h1>
+
+<div class="T-Shirt">
+    <h2><?= $products[0]["Name"] ?></h2>
+    <p class="prix"><?= $products[0]["Price"] ?> €</p>
+    <p class="stock">Stock : <?= $products[0]["Stock"] ?></p>
+</div>
+
+<div class="Jean">
+    <h2><?= $products[1]["Name"] ?></h2>
+    <p class="prix"><?= $products[1]["Price"] ?> €</p>
+    <p class="stock">Stock : <?= $products[1]["Stock"] ?></p>
+</div>
+
+<div class="Casquette">
+    <h2><?= $products[2]["Name"] ?></h2>
+    <p class="prix"><?= $products[2]["Price"] ?> €</p>
+    <p class="stock">Stock : <?= $products[2]["Stock"] ?></p>
+</div>
+
+<div class="Veste">
+    <h2><?= $products[3]["Name"] ?></h2>
+    <p class="prix"><?= $products[3]["Price"] ?> €</p>
+    <p class="stock">Stock : <?= $products[3]["Stock"] ?></p>
+</div>
+
+<div class="Echarpe">
+    <h2><?= $products[4]["Name"] ?></h2>
+    <p class="prix"><?= $products[4]["Price"] ?> €</p>
+    <p class="stock">Stock : <?= $products[4]["Stock"] ?></p>
+</div>
+
+<div class="Gants">
+    <h2><?= $products[5]["Name"] ?></h2>
+    <p class="prix"><?= $products[5]["Price"] ?> €</p>
+    <p class="stock">Stock : <?= $products[5]["Stock"] ?></p>
+</div>
+
+
     </body>
 
 </html>
--- exercices/jour-02/page-catalogue.php
+++ exercices/jour-02/page-catalogue.php
@@ -1,81 +1,53 @@
+<?php
+// starter-project/public/catalogue.php
+require_once __DIR__ . '/../app/data.php';
+// $products est maintenant disponible
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <title>Test PHP</title>
     </head>
     <body>
-        <?php 
-    $products = [
-    [
-        "name" => "T-shirt",
-        "price" => 29.99,
-        "stock" => 50
-    ],
-    [
-        "name" => "Jean",
-        "price" => 79.99,
-        "stock" => 30
-    ],
-    [
-        "name" => "Casquette",
-        "price" => 19.99,
-        "stock" => 100
-    ],
-    [
-        "name" => "Veste",
-        "price" => 49.99,
-        "stock" => 80
-    ],
-    [
-        "name" => "Echarpe",
-        "price" => 10.99,
-        "stock" => 40
-    ],
 
-    [
-        "name" => "Gants",
-        "price" => 15.99,
-        "stock" => 20
-    ]
-    ]
-        ?>
 
 <h1>Consultez nos produits</h1>
 
 <div class="T-Shirt">
-    <h2><?= $products[0]["name"] ?></h2>
-    <p class="prix"><?= $products[0]["price"] ?> €</p>
-    <p class="stock">Stock : <?= $products[0]["stock"] ?></p>
+    <h2><?= $products[0]["Name"] ?></h2>
+    <p class="prix"><?= $products[0]["Price"] ?> €</p>
+    <p class="stock">Stock : <?= $products[0]["Stock"] ?></p>
 </div>
 
 <div class="Jean">
-    <h2><?= $products[1]["name"] ?></h2>
-    <p class="prix"><?= $products[1]["price"] ?> €</p>
-    <p class="stock">Stock : <?= $products[1]["stock"] ?></p>
+    <h2><?= $products[1]["Name"] ?></h2>
+    <p class="prix"><?= $products[1]["Price"] ?> €</p>
+    <p class="stock">Stock : <?= $products[1]["Stock"] ?></p>
 </div>
 
 <div class="Casquette">
-    <h2><?= $products[2]["name"] ?></h2>
-    <p class="prix"><?= $products[2]["price"] ?> €</p>
-    <p class="stock">Stock : <?= $products[2]["stock"] ?></p>
+    <h2><?= $products[2]["Name"] ?></h2>
+    <p class="prix"><?= $products[2]["Price"] ?> €</p>
+    <p class="stock">Stock : <?= $products[2]["Stock"] ?></p>
 </div>
 
 <div class="Veste">
-    <h2><?= $products[3]["name"] ?></h2>
-    <p class="prix"><?= $products[3]["price"] ?> €</p>
-    <p class="stock">Stock : <?= $products[3]["stock"] ?></p>
+    <h2><?= $products[3]["Name"] ?></h2>
+    <p class="prix"><?= $products[3]["Price"] ?> €</p>
+    <p class="stock">Stock : <?= $products[3]["Stock"] ?></p>
 </div>
 
 <div class="Echarpe">
-    <h2><?= $products[4]["name"] ?></h2>
-    <p class="prix"><?= $products[4]["price"] ?> €</p>
-    <p class="stock">Stock : <?= $products[4]["stock"] ?></p>
+    <h2><?= $products[4]["Name"] ?></h2>
+    <p class="prix"><?= $products[4]["Price"] ?> €</p>
+    <p class="stock">Stock : <?= $products[4]["Stock"] ?></p>
 </div>
 
 <div class="Gants">
-    <h2><?= $products[5]["name"] ?></h2>
-    <p class="prix"><?= $products[5]["price"] ?> €</p>
-    <p class="stock">Stock : <?= $products[5]["stock"] ?></p>
+    <h2><?= $products[5]["Name"] ?></h2>
+    <p class="prix"><?= $products[5]["Price"] ?> €</p>
+    <p class="stock">Stock : <?= $products[5]["Stock"] ?></p>
 </div>
 
 
